1148daeb9fcee6b210da1a8ac4ad875b072e868f
Eric LIAO <d1044171009@gmail.com>
2022-08-28 03:25:24 +0800

    feat: 新增 record model

---
+++ models/record.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const recordSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  categoryId: {
+    type: Number,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+})
+
+module.exports = mongoose.model('Record', recordSchema)
--- routes/modules/ledger.js
+++ routes/modules/ledger.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
+// 引入 record model
+const Record = require('../../models/record')
+
+//{ name: 'eric', date: '2022-08-01', category: '4', amount: '2' }
+
 router.get('/new', (req, res) => {
   res.render('new')
 })
 
 router.post('/new', (req, res) => {
-  console.log(req.body)
-  res.redirect('/')
-})
+  const { name, date, category, amount } = req.body
+  return Record.create({ name, date, categoryId: category, amount})
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+  })
 
 module.exports = router
