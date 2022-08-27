c97ed5009eff5954e9ea05b36f2c0840181cad22
Eric LIAO <d1044171009@gmail.com>
2022-08-27 23:51:04 +0800

    feat: 新增佈局基本樣式

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 
 const app = express()
-const PORT = 3000
+const PORT = 3001
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs') 
+
+
+
+
+
 
 app.get('/', (req, res) => {
-  res.send('This is test')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
