b2e3dfc96b429a8658e1329c512b32954be4d4b6
Eric LIAO <d1044171009@gmail.com>
2022-08-29 01:19:37 +0800

    feat: 新增使用者註冊功能

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -11,6 +13,35 @@ router.post('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+
+  // 檢查是否註冊
+  User.findOne({ email })
+    .then(user => {
+      // 已經註冊過了
+      if (user) {
+        console.log('this email already exists.')
+        res.render('register', {
+          name, 
+          email, 
+          password, 
+          confirmPassword
+        })
+      } else {
+        // 還沒註冊過 新增
+        return User.create({ 
+          name,
+          email,
+          password
+        })
+          .then(() => res.redirect('/users/login'))
+          .catch(err => console.log(err))
+      }
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
