fe64ad1c6e00a76a5e774c0e12451c7163c6f502
Eric LIAO <d1044171009@gmail.com>
2022-08-29 02:06:01 +0800

    feat: add user authentication in home and ledger routers

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,10 @@ const home = require('./modules/home')
 const ledger = require('./modules/ledger')
 const users = require('./modules/users')
 
-router.use('/', home)
-router.use('/ledger', ledger)
+const { authenticator } = require('../middleware/auth') // 把驗證登入狀態 middleware載入 
+
+router.use('/ledger', authenticator, ledger)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 module.exports = router
