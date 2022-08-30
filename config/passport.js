f2b9ebafa200d710e7ed31e964496d39d16a7679
Eric LIAO <d1044171009@gmail.com>
2022-08-30 15:07:27 +0800

    feat: 修改edit頁面

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
 
 const User = require('../models/user')
 
@@ -15,10 +16,13 @@ module.exports = (app) => {
         if (!user) {
           return done(null, false, { message: '此 Email 還沒有註冊！'})
         }
-        if (user.password !== password) {
-          return done(null, false, { message: "Email 或密碼錯誤！"})
-        }
-        return done(null, user)
+        return bcrypt.compare(password, user.password)
+          .then(isMatch => {
+            if (!isMatch) {
+              return done(null, false, { message: "Email 或密碼錯誤！"})
+            }
+            return done(null, user)
+          })
       })
       .catch(err => done(err, false))
   }))
