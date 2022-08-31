aaab665fd0755dd55748144cef0ffdb2a6fb5bf4
Eric LIAO <d1044171009@gmail.com>
2022-08-31 15:41:42 +0800

    feat: 修改更新資料路由

--- routes/modules/ledger.js
+++ routes/modules/ledger.js
@@ -4,6 +4,7 @@ const router = express.Router()
 // 引入 record model
 const Record = require('../../models/record')
 const Category = require('../../models/category')
+const category = require('../../models/category')
 
 //{ name: 'eric', date: '2022-08-01', category: '4', amount: '2' }
 
@@ -11,7 +12,6 @@ router.get('/new', (req, res) => {
   res.render('new')
 })
 
-// { name: '買蘋果', date: '2022-09-01', category: '4', amount: '12' }
 router.post('/new', (req, res) => {
   const userId = req.user._id
   const { name, date, categoriesId, amount } = req.body
@@ -32,15 +32,27 @@ router.get('/:id/edit', (req, res) => {
   let _id = req.params.id
   Record.findOne({ _id, userId })
     .lean()
-    .then(record => res.render('edit', { record }))
-    .catch(err => console.log(err))
+    .then(record => {
+      res.render('edit', { record })
+    })
 })
 
 // 更新資料路由
 router.put('/:id', (req, res) => {
   const userId = req.user._id
   const _id = req.params.id
-  return Record.findOneAndUpdate({ _id, userId }, { ...req.body })
+  const id = req.body.categoriesId
+  Category.findOne({ id })
+    .then(category => {
+      const category_id = category._id
+      const { name, date, amount } = req.body
+      return Record.findOneAndUpdate({ _id, userId }, {
+        name,
+        date,
+        amount,
+        categoryId: category_id
+      })
+    })
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
 })
