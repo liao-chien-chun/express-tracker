f36c4342647d9a70cdd7d1576853353fba9aa189
Eric LIAO <d1044171009@gmail.com>
2022-08-31 17:30:53 +0800

    feat; 選擇分類重新渲染畫面

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,21 +2,46 @@ const express = require('express')
 const router = express.Router()
 
 const Record = require('../../models/record')
+const Category = require('../../models/category')
+const category = require('../../models/category')
 
 router.get('/', (req, res) => {
   const userId = req.user._id
+  const select = req.query.select
+  let title = '類別'
   let totalAmount = 0
-  Record.find({ userId })
+  if(!select) {
+    Record.find({ userId })
     .lean()
     .sort({ _id: 'asc' })
     .then(records =>{ 
       records.filter(record => {
         totalAmount += record.amount
       })
-      res.render('index', { records, totalAmount })
+      return res.render('index', { records, totalAmount, title })
     })
     .catch(err => console.log(err))
+  } else {
+    Category.findOne({ id: select })
+      .then(category => {
+        const categoryId = category._id
+        const categoryName = category.name
+        let title = categoryName
+        Record.find({ categoryId })
+          .lean()
+          .then(records => {
+            records.filter(record => {
+              totalAmount += record.amount
+            })
+             return res.render('index', { records, totalAmount, title })
+          })
+      })
+  }
 })
+
+
+  
+
 
 
 module.exports = router
