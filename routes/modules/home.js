75640b126054623e232df0024fd7e640da073f9b
Eric LIAO <d1044171009@gmail.com>
2022-08-31 15:58:05 +0800

    feat: 修改首頁路由讓總金額自動加總與顯示

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,10 +5,16 @@ const Record = require('../../models/record')
 
 router.get('/', (req, res) => {
   const userId = req.user._id
+  let totalAmount = 0
   Record.find({ userId })
     .lean()
     .sort({ _id: 'asc' })
-    .then(records => res.render('index', { records }))
+    .then(records =>{ 
+      records.filter(record => {
+        totalAmount += record.amount
+      })
+      res.render('index', { records, totalAmount })
+    })
     .catch(err => console.log(err))
 })
 
